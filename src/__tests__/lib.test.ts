31970d19c3806d4ef9e8b54e9f3aa45f01c49441
Johannes Kühlewindt <johannes.kuehlewindt@klarna.com>
2019-11-17 21:22:57 +0100

    add offset function

--- src/__tests__/lib.test.ts
+++ src/__tests__/lib.test.ts
@@ -1,4 +1,9 @@
-import { render, createRect, compose } from '../index'
+import { map, createRect, compose } from '../index'
+
+test('map', () => {
+    const result = map([1, 1, 1, 1], (el, i, arr) => el * i * arr.length)
+    expect(result).toEqual([0, 4, 8, 12])
+})
 
 describe('The Lib', () => {
     it('can create a rect', () => {
@@ -7,18 +12,34 @@ describe('The Lib', () => {
             'xxx',
         ])
     })
-
     it('can compose 2 elements', () => {
         const canvas = createRect({ width: 5, height: 4, char: 'X' })
         const rect = createRect({ width: 3, height: 2, char: '0' })
-        expect(compose(canvas, rect).join('\n')).toBe(5)
+        expect(compose(canvas, rect)).toEqual([
+            '000XX',
+            '000XX',
+            'XXXXX',
+            'XXXXX',
+        ])
     })
-    // it('renders str in container', () => {
-    //     const canvas =
-
-    //     const result = render()
-
-    //     expect(result).toBe(5)
-    // })
-    it.todo('renders str in container (with offset)')
+    it('can compose 2 elements with x offset', () => {
+        const canvas = createRect({ width: 5, height: 4, char: 'X' })
+        const rect = createRect({ width: 3, height: 2, char: '0' })
+        expect(compose(canvas, rect, { x: 1 })).toEqual([
+            'X000X',
+            'X000X',
+            'XXXXX',
+            'XXXXX',
+        ])
+    })
+    it('can compose 2 elements with x and y offset', () => {
+        const canvas = createRect({ width: 5, height: 4, char: 'X' })
+        const rect = createRect({ width: 3, height: 2, char: '0' })
+        expect(compose(canvas, rect, { x: 1, y: 2 })).toEqual([
+            'XXXXX',
+            'XXXXX',
+            'X000X',
+            'X000X',
+        ])
+    })
 })
--- src/index.ts
+++ src/index.ts
@@ -23,21 +23,38 @@ interface Coordinate {
     y: number
 }
 
+interface CoordinateOption {
+    x?: number
+    y?: number
+}
+
+export const map = <T, K>(
+    arr: Array<T>,
+    fun: (el: T, i: number, arr: Array<T>) => K
+): Array<K> => {
+    const result: Array<K> = []
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = fun(arr[i], i, arr)
+    }
+    return result
+}
+
 export const compose = (
     bottom: string[],
     top: string[],
-    offset: Coordinate = { x: 0, y: 0 }
+    offset: CoordinateOption = { x: 0, y: 0 }
 ) => {
-    const result = bottom.slice()
-    for (let y = 0; y < result.length; y++) {
-        const tempStr = result[y].split('')
-        for (let x = 0; x < tempStr.length; x++) {
-            if (top[y] && top[y][x]) {
-                tempStr[x] = top[y][x]
-            }
-        }
-        result[y] = tempStr.join('')
-    }
+    const xOffset = offset.x || 0
+    const yOffset = offset.y || 0
+
+    const result = map(bottom, (bottomLine, y) =>
+        map(
+            bottomLine.split('').slice(),
+            (bottomChar, x) =>
+                (top[y - yOffset] && top[y - yOffset][x - xOffset]) ||
+                bottomChar
+        ).join('')
+    )
     return result
 }
 
