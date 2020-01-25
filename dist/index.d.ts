ce7e4a57e5af683ed75cdb7a60c8405597ff9693
Johannes Kühlewindt <johannes.kuehlewindt@klarna.com>
2020-01-25 23:40:25 +0100

    built

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,33 +1,26 @@
-export { map, reduce, forI } from './util/array'
-export declare type ImageLine = string
-export declare type Image = ImageLine[]
+export { map, reduce, forI } from './util/array';
+export declare type ImageLine = string;
+export declare type Image = ImageLine[];
 export interface Dimension {
-    width: number
-    height: number
+    width: number;
+    height: number;
 }
 export interface Coordinate {
-    x: number
-    y: number
+    x: number;
+    y: number;
 }
-export declare type CropInput = Partial<Dimension & Coordinate>
+export declare type CropInput = Partial<Dimension & Coordinate>;
 declare type CreateRectOptions = Dimension & {
-    char: string
-}
-export declare const createRect: ({
-    width,
-    height,
-    char,
-}: CreateRectOptions) => Image
+    char: string;
+};
+export declare const createRect: ({ width, height, char, }: CreateRectOptions) => Image;
 /**
  * takes an image and subsitutes a character (default " ") with \u0000 which is
  * treated as transparancy in composing images
  * @param image
  * @param charToSubstitute
  */
-export declare const toTransparency: (
-    image: Image,
-    charToSubstitute?: string
-) => string[]
+export declare const toTransparency: (image: Image, charToSubstitute?: string) => string[];
 /**
  * takes an images and applies the other one on top resulting in a new image
  * with the dimensions of the bottom one
@@ -35,25 +28,18 @@ export declare const toTransparency: (
  * @param top
  * @param offset
  */
-export declare const compose: (
-    bottom: Image,
-    top: Image,
-    offset?: Partial<Coordinate>
-) => string[]
+export declare const compose: (bottom: Image, top: Image, offset?: Partial<Coordinate>) => string[];
 /**
  * creates new image from offset and dimension
  * @param image
  * @param param1
  */
-export declare const crop: (
-    image: Image,
-    cropInput: Partial<Dimension & Coordinate>
-) => Image
+export declare const crop: (image: Image, cropInput: Partial<Dimension & Coordinate>) => Image;
 /**
  * rotates an image to the right, as often as specified by the number parameter
  * @param image
  * @param n
  */
-export declare const rotate: (image: Image, n?: number) => Image
-export declare const isInBounds: (img: Image, c: Coordinate) => boolean
-export declare const measure: (image: Image) => Dimension
+export declare const rotate: (image: Image, n?: number) => Image;
+export declare const measure: (image: Image) => Dimension;
+export declare const isInBounds: (img: Image, c: Coordinate) => boolean;
